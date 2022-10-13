742afb9eae68cd7e79ef935e7929e875a2d301cf
budawu <1183972971@qq.com>
2022-10-13 20:55:02 +0800

    v0.3.1添加设置

---
+++ src/settings.py
@@ -0,0 +1,16 @@
+from colorama import Fore
+
+VERSION = 'v0.3.0'
+
+START = '''
+____   _   _____            _ _ 
+|  _ \(_) / ___|| |__   ___| | |
+| |_) | | \___ \| '_ \ / _ \ | |
+|  __/| |  ___) | | | |  __/ | |
+|_|   |_| |____/|_| |_|\___|_|_|
+    '''
+f'Pi-shell {VERSION}\n'
+' Type "exit" or Ctrl+C to exit.'
+
+PATHCOLOR = Fore.YELLOW
+PROMPTCOLOR = Fore.BLUE
